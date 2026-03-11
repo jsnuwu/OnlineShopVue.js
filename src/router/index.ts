512668b16b113cafd9cd5ce3680566c44a190dda
jsnuwu <jasonbay05@gmail.com>
2026-03-11 14:44:52 +0100

    deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ProductDetails from '../views/ProductDetails.vue'
 import Cart from '../views/Cart.vue'
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/OnlineShopVue.js/'),
   routes,
 })
 
